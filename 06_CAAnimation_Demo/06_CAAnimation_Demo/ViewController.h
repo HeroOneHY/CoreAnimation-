dd811bd2edcedba91c8f82c9fb399275415b6e8e
[1510360132@qq.com] <1510360132@qq.com>
2018-09-28 19:13:09 +0800

    init

---
+++ 06_CAAnimation_Demo/06_CAAnimation_Demo/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  06_CAAnimation_Demo
+//
+//  Created by baoxu on 16/3/9.
+//  Copyright © 2016年 BaoXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    NSInteger _transtionIndex;
+}
+
+@end
+
